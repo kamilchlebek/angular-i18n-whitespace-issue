aa0d98a547c7ddae8aa5991df9a45f141e2f85d2
Kamil Chlebek <kamil.chlebek94@gmail.com>
2020-02-28 13:47:10 +0100

    Issue demo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { async, TestBed } from '@angular/core/testing'
+import { AppComponent } from './app.component'
+import { By } from '@angular/platform-browser'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -10,22 +11,19 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have as title 'i18n-whitespace'`, () => {
+  it('should show days suffix if label type is days', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('i18n-whitespace');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    component.type = 'days';
+    component.value = 10;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('i18n-whitespace app is running!');
-  });
+
+    const span = fixture.debugElement.query(By.css('span.test'));
+
+    console.log('span.nativeElement', span.nativeElement, '|')
+    console.log('span.nativeElement.textContent.trim()', span.nativeElement.textContent.trim(), '|')
+
+    expect(span.nativeElement.textContent.trim()).toEqual('10 days');
+  })
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'i18n-whitespace';
+  type = 'days';
+  value = 1;
 }
